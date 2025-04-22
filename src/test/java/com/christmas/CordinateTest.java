ef06f5fa18749ed1a2f3c69f4aa21265e48a7ee7
Mike27-18 <mdnslugger@gmail.com>
2025-04-22 19:33:41 +0000

    test: MakeCordinatePair

---
+++ src/test/java/com/christmas/CordinateTest.java
@@ -0,0 +1,14 @@
+package com.christmas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+
+import java.beans.Transient;
+
+public class CordinateTest{
+    @Test
+    void MakeCordinatePair(){
+        CordinatePair pair = new CordinatePair();
+    }
+}
