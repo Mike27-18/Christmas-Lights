098f3496fdd624e4dd3c4c14a6e364267b77dcd0
Mike27-18 <mdnslugger@gmail.com>
2025-04-25 22:32:55 +0000

    test: SingleLightTest

---
+++ src/test/java/com/christmas/LightTest.java
@@ -0,0 +1,13 @@
+package com.christmas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+
+import java.beans.Transient;
+public class LightTest {
+    @Test
+    void SingleLightTest(){
+        Light light = new Light();
+    }
+}
